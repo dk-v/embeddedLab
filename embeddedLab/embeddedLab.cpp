f2b8b4ddbce6f627f1d910ee5510409a9e032359
David Khang Vu <dk-vu@web.de>
2022-03-30 15:50:35 +0200

    read file from command line

--- embeddedLab/embeddedLab.cpp
+++ embeddedLab/embeddedLab.cpp
@@ -1,20 +1,36 @@
-// embeddedLab.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
-//
-
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main(int argc, char* argv[]) {
+    //string fileName = argv[1];
+    string fileName = "gps_sequence_8.txt";
+    /*fileName += ".txt";
+    cout << fileName;*/
+    // attach an input stream to the wanted file
+    ifstream input_stream(fileName);
+
+    // check stream status
+    if (!input_stream) cerr << "Can't open input file!";
+
+    // file contents  
+    vector<string> text;
+
+    // one line
+    string line;
+
+    // extract all the text from the input file
+    while (getline(input_stream, line)) {
+
+        // store each line in the vector
+        text.push_back(line);
+    }
+    for (auto i : text)
+        std::cout << i << ' ';
+
+
+
+    return 0;
 }
-
-// Programm ausführen: STRG+F5 oder Menüeintrag "Debuggen" > "Starten ohne Debuggen starten"
-// Programm debuggen: F5 oder "Debuggen" > Menü "Debuggen starten"
-
-// Tipps für den Einstieg: 
-//   1. Verwenden Sie das Projektmappen-Explorer-Fenster zum Hinzufügen/Verwalten von Dateien.
-//   2. Verwenden Sie das Team Explorer-Fenster zum Herstellen einer Verbindung mit der Quellcodeverwaltung.
-//   3. Verwenden Sie das Ausgabefenster, um die Buildausgabe und andere Nachrichten anzuzeigen.
-//   4. Verwenden Sie das Fenster "Fehlerliste", um Fehler anzuzeigen.
-//   5. Wechseln Sie zu "Projekt" > "Neues Element hinzufügen", um neue Codedateien zu erstellen, bzw. zu "Projekt" > "Vorhandenes Element hinzufügen", um dem Projekt vorhandene Codedateien hinzuzufügen.
-//   6. Um dieses Projekt später erneut zu öffnen, wechseln Sie zu "Datei" > "Öffnen" > "Projekt", und wählen Sie die SLN-Datei aus.
